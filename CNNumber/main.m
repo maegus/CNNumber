65887bf645d1497db8ebecb5d649c7451e9b59f4
Draveness <stark.draven@gmail.com>
2015-04-17 15:00:45 +0800

    Initial Commit

---
+++ CNNumber/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CNNumber
+//
+//  Created by apple on 4/17/15.
+//  Copyright (c) 2015 DeltaX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
